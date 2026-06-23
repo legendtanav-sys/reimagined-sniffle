b36526630446660b4ee8ba00c3feee4d0a99841d
legendtanav-sys <legend.tanav@gmail.com>
2026-06-23 16:49:37 +0530

    Add files via upload

---
+++ ARRAYSSS.py
@@ -0,0 +1,11 @@
+import array as arr
+
+#create 1
+array_num = arr.array('i',[1,3,5,3,7,9])
+print("original array:"+str(array_num))
+#COUNT
+print("I WONT WRITE THIS BC MAM IS LATE"+str(array_num.count(3)))
+#reverse i dont know what we will reverse but we will reverse it!
+array_num.reverse()
+print("I DONT KNOWW!!!")
+print(str(array_num))
