45b3ea7c38e3c5f3cf56e04039146082e3e6b465
legendtanav-sys <legend.tanav@gmail.com>
2026-06-23 16:36:18 +0530

    Add files via upload

---
+++ i dunno.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("Original JUST Original")
+print(setx)
+print(sety)
+print("\nIntersection of 2 said sets")
+setz = setx.intersection(sety)
+print(setz)
